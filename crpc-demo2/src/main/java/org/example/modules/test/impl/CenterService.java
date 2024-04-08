74015f87f0afd77907dae5f69ff13b151c8f982f
Shangquan <2840927713@qq.com>
2024-04-08 22:17:25 +0800

    fix ~

--- crpc-demo2/src/main/java/org/example/modules/test/impl/CenterService.java
+++
@@ -1,9 +0,0 @@
-package org.example.modules.test.impl;
-
-import chen.shangquan.crpc.network.data.RpcRequest;
-import chen.shangquan.crpc.server.annotation.ServerRegister;
-
-@ServerRegister(serverName = "CrpcServer", className = "CenterService")
-public interface CenterService {
-    public Object dealMethod(RpcRequest request);
-}
